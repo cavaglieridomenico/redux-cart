1326feb1c657ca0d000909aed45f9674c917233f
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-04-16 06:40:05 +0200

    [FIX] CartItem.js

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import { REMOVE, TOGGLE_AMOUNT } from "../actions";
 
 const CartItem = ({ img, title, price, amount, remove, toggle }) => {
